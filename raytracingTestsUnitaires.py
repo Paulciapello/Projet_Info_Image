d3dd3a234562850e5c88d88eb8ae6f0cca20f24f
ciapello <127536863+Paulciapello@users.noreply.github.com>
2023-11-24 16:39:07 +0100

    1ère partie terminé

--- raytracingTestsUnitaires.py
+++ raytracingTestsUnitaires.py
@@ -1,61 +1,91 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import numpy as np
-
+import math
 import numpy.testing as nptest
-
 
 w = 800
 h = 600
 
-img = np.zeros((h, w, 3)) # image vide : que du noir
-        
-C = np.array([0., 0.1, 1.1])  # Coordonée du centre de la camera.
-Q = np.array([0,0.3,0])  # Orientation de la caméra
-img = np.zeros((h, w, 3)) # image vide : que du noir
+img = np.zeros((h, w, 3))  # image vide : que du noir
+
+C = np.array([0., 0.1, 1.1])  # Coordonnée du centre de la camera.
+Q = np.array([0, 0.3, 0])  # Orientation de la caméra
+img = np.zeros((h, w, 3))  # image vide : que du noir
 
 r = float(w) / h
 # coordonnées de l'écran : x0, y0, x1, y1.
-S = (-1., -1. / r , 1., 1. / r )
+S = (-1., -1. / r, 1., 1. / r)
 
 
 def create_Ray(O, D):
-    ray={'origine':np.array(D),
-       'direction':np.array(O)}
+    ray = {'origin': np.array(O),
+           'direction': np.array(D)}
     return ray
 
+
 def create_sphere(P, r, i):
-    sphere={'type':'sphere',
-            'centre':np.array(P),
-            'rayon':np.array(r),
-            'index_sphere':int(i)}
+    sphere = {'type': 'sphere',
+              'centre': np.array(P),
+              'rayon': np.array(r),
+              'index_sphere': int(i)}
     return sphere
 
+
 def create_plane(P, n, i):
-    plane={'position':np.array(P),
-            'vect_n':np.array(r),
-            'index_plane':int(i)}
+    plane = {'position': np.array(P),
+             'vect_n': np.array(n),
+             'index_plane': int(i)}
+    return plane
+
 
 def normalize(x):
-    return x/np.linalg.norm(x)
+    return x / np.linalg.norm(x)
 
-def rayAt(ray,t):
+
+def rayAt(ray, t):
     O = ray['origin']
     D = ray['direction']
-    return O + t*D
+    return O + t * D
+
 
 def intersect_Plane(ray, plane):
-        P = plane['position']
-        n = plane['vect_n']
-        if abs( np.dot(D,n))<1e-6:
+    P = plane['position']
+    n = plane['vect_n']
+    O = ray['origin']
+    D = ray['direction']
+    if abs(np.dot(P, n)) < 1e-6:
+        return np.inf
+    else:
+        t = np.dot((P - O), n) / np.dot(D, n)
+        if t > 0:
+            return t
+        else:
             return np.inf
-        t = -np.dot((O-P,r))/np.dot(D,n)
-    
-    return   
+
 
 def intersect_Sphere(ray, sphere):
-    # Remplissez ici 
-    
+    O = ray['origin']
+    D = ray['direction']
+    P = sphere['centre']
+    r = sphere['rayon']
+
+    a = np.dot(D, D)
+    b = -2 * np.dot(D, (P - O))
+    c = np.dot((P - O), (P - O)) - r**2
+
+    delta = b**2 - 4 * a * c
+    if delta >= 0:
+        t1 = (-b - math.sqrt(delta)) / (2 * a)
+        t2 = (-b + math.sqrt(delta)) / (2 * a)
+        if t1 >= 0 and t2 >= 0:
+            return min(t1, t2)
+        else:
+            return np.inf
+    else:
+        return np.inf
+
+       
 #Test unitaire de la fonction codage. Si un test est faux, le message d'erreur vous dira quel test ne passe pas 
 def test_traceRay():
     r1 = create_Ray(np.array([0., 0.1, 1.1]), np.array([ 0.08461529, -0.09804421, -0.99157833]))
